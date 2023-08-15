044f48547467525745e6db22caecc0d41156da1e
Tim Gabrikowski <timgabrikowski@outlook.de>
2023-08-15 16:37:01 +0200

    booksapi display

--- booksapi.js
+++ booksapi.js
@@ -1,36 +1,49 @@
-async function get_api_suggestions(cur)
-{
+async function get_api_suggestions(cur) {
 	let search = cur.title + " " + cur.subtitle;
 
 	const response = await fetch(
 		"https://www.googleapis.com/books/v1/volumes?" +
-			new URLSearchParams(
-				{
-					q: search
-				}
-			));
+			new URLSearchParams({
+				q: search,
+			})
+	);
 
 	const json = await response.json();
 
-	return json.items.map(item => {
-		const isbn10 = item.volumeInfo.industryIdentifiers.find(el => el.type == "ISBN_10");
-		const isbn13 = item.volumeInfo.industryIdentifiers.find(el => el.type == "ISBN_13");
+	return json.items.map((item) => {
+		const isbn10 = item.volumeInfo.industryIdentifiers.find(
+			(el) => el.type == "ISBN_10"
+		);
+		const isbn13 = item.volumeInfo.industryIdentifiers.find(
+			(el) => el.type == "ISBN_13"
+		);
 
 		return {
 			title: item.volumeInfo.title,
 			subtitle: "",
-			author: item.volumeInfo.authors != null ?
-				item.volumeInfo.authors.join(", ") : "",
-			publisher: item.volumeInfo.publisher != null ?
-				item.volumeInfo.publisher : "",
-			year: item.volumeInfo.publishedDate != null ?
-				parseInt(item.volumeInfo.publishedDate.split("-")[0]) : -1,
-			isbn: isbn13 != null ? isbn13.identifier : isbn10.identifier,
-			pages: item.volumeInfo.pageCount != null ?
-				item.volumeInfo.pageCount : -1,
-			description: item.volumeInfo.description != null ?
-				item.volumeInfo.description : "",
-			image: item.volumeInfo.imageLinks != null ?
-				item.volumeInfo.imageLinks.thumbnail : null
-		}});
+			author:
+				item.volumeInfo.authors != null
+					? item.volumeInfo.authors.join(", ")
+					: "",
+			publisher:
+				item.volumeInfo.publisher != null ? item.volumeInfo.publisher : "",
+			year:
+				item.volumeInfo.publishedDate != null
+					? parseInt(item.volumeInfo.publishedDate.split("-")[0])
+					: -1,
+			isbn:
+				isbn13 != null
+					? isbn13.identifier
+					: isbn10 != null
+					? isbn10.identifier
+					: "",
+			pages: item.volumeInfo.pageCount != null ? item.volumeInfo.pageCount : -1,
+			description:
+				item.volumeInfo.description != null ? item.volumeInfo.description : "",
+			image:
+				item.volumeInfo.imageLinks != null
+					? item.volumeInfo.imageLinks.thumbnail
+					: null,
+		};
+	});
 }
--- script.js
+++ script.js
@@ -1,115 +1,101 @@
+function _min(d0, d1, d2, bx, ay) {
+	return d0 < d1 || d2 < d1
+		? d0 > d2
+			? d2 + 1
+			: d0 + 1
+		: bx === ay
+		? d1
+		: d1 + 1;
+}
 
-  function _min(d0, d1, d2, bx, ay)
-  {
-    return d0 < d1 || d2 < d1
-        ? d0 > d2
-            ? d2 + 1
-            : d0 + 1
-        : bx === ay
-            ? d1
-            : d1 + 1;
-  }
+function levenshtein(a, b) {
+	if (a === b) {
+		return 0;
+	}
 
-  function levenshtein(a, b)
-  {
-    if (a === b) {
-      return 0;
-    }
+	if (a.length > b.length) {
+		var tmp = a;
+		a = b;
+		b = tmp;
+	}
 
-    if (a.length > b.length) {
-      var tmp = a;
-      a = b;
-      b = tmp;
-    }
+	var la = a.length;
+	var lb = b.length;
 
-    var la = a.length;
-    var lb = b.length;
+	while (la > 0 && a.charCodeAt(la - 1) === b.charCodeAt(lb - 1)) {
+		la--;
+		lb--;
+	}
 
-    while (la > 0 && (a.charCodeAt(la - 1) === b.charCodeAt(lb - 1))) {
-      la--;
-      lb--;
-    }
+	var offset = 0;
 
-    var offset = 0;
+	while (offset < la && a.charCodeAt(offset) === b.charCodeAt(offset)) {
+		offset++;
+	}
 
-    while (offset < la && (a.charCodeAt(offset) === b.charCodeAt(offset))) {
-      offset++;
-    }
+	la -= offset;
+	lb -= offset;
 
-    la -= offset;
-    lb -= offset;
+	if (la === 0 || lb < 3) {
+		return lb;
+	}
 
-    if (la === 0 || lb < 3) {
-      return lb;
-    }
+	var x = 0;
+	var y;
+	var d0;
+	var d1;
+	var d2;
+	var d3;
+	var dd;
+	var dy;
+	var ay;
+	var bx0;
+	var bx1;
+	var bx2;
+	var bx3;
 
-    var x = 0;
-    var y;
-    var d0;
-    var d1;
-    var d2;
-    var d3;
-    var dd;
-    var dy;
-    var ay;
-    var bx0;
-    var bx1;
-    var bx2;
-    var bx3;
+	var vector = [];
 
-    var vector = [];
+	for (y = 0; y < la; y++) {
+		vector.push(y + 1);
+		vector.push(a.charCodeAt(offset + y));
+	}
 
-    for (y = 0; y < la; y++) {
-      vector.push(y + 1);
-      vector.push(a.charCodeAt(offset + y));
-    }
+	var len = vector.length - 1;
 
-    var len = vector.length - 1;
+	for (; x < lb - 3; ) {
+		bx0 = b.charCodeAt(offset + (d0 = x));
+		bx1 = b.charCodeAt(offset + (d1 = x + 1));
+		bx2 = b.charCodeAt(offset + (d2 = x + 2));
+		bx3 = b.charCodeAt(offset + (d3 = x + 3));
+		dd = x += 4;
+		for (y = 0; y < len; y += 2) {
+			dy = vector[y];
+			ay = vector[y + 1];
+			d0 = _min(dy, d0, d1, bx0, ay);
+			d1 = _min(d0, d1, d2, bx1, ay);
+			d2 = _min(d1, d2, d3, bx2, ay);
+			dd = _min(d2, d3, dd, bx3, ay);
+			vector[y] = dd;
+			d3 = d2;
+			d2 = d1;
+			d1 = d0;
+			d0 = dy;
+		}
+	}
 
-    for (; x < lb - 3;) {
-      bx0 = b.charCodeAt(offset + (d0 = x));
-      bx1 = b.charCodeAt(offset + (d1 = x + 1));
-      bx2 = b.charCodeAt(offset + (d2 = x + 2));
-      bx3 = b.charCodeAt(offset + (d3 = x + 3));
-      dd = (x += 4);
-      for (y = 0; y < len; y += 2) {
-        dy = vector[y];
-        ay = vector[y + 1];
-        d0 = _min(dy, d0, d1, bx0, ay);
-        d1 = _min(d0, d1, d2, bx1, ay);
-        d2 = _min(d1, d2, d3, bx2, ay);
-        dd = _min(d2, d3, dd, bx3, ay);
-        vector[y] = dd;
-        d3 = d2;
-        d2 = d1;
-        d1 = d0;
-        d0 = dy;
-      }
-    }
+	for (; x < lb; ) {
+		bx0 = b.charCodeAt(offset + (d0 = x));
+		dd = ++x;
+		for (y = 0; y < len; y += 2) {
+			dy = vector[y];
+			vector[y] = dd = _min(dy, d0, dd, bx0, vector[y + 1]);
+			d0 = dy;
+		}
+	}
 
-    for (; x < lb;) {
-      bx0 = b.charCodeAt(offset + (d0 = x));
-      dd = ++x;
-      for (y = 0; y < len; y += 2) {
-        dy = vector[y];
-        vector[y] = dd = _min(dy, d0, dd, bx0, vector[y + 1]);
-        d0 = dy;
-      }
-    }
-
-    return dd;
-  };
-
-
-
-
-
-
-
-
-
-
-
+	return dd;
+}
 
 const cont_file_upload = elem("#cont_file_upload");
 const cont_cur_book_data = elem("#cont_cur_book_data");
@@ -133,25 +119,21 @@ const cont_suggestions = elem("#cont_suggestions");
 
 let data = null;
 
-function elem(id)
-{
+function elem(id) {
 	let elem = document.querySelector(id);
-	if(elem == null) console.log("INVALID ID: " + id);
+	if (elem == null) console.log("INVALID ID: " + id);
 	return elem;
 }
 
-function hide(el)
-{
+function hide(el) {
 	el.style.display = "none";
 }
 
-function show(el)
-{
+function show(el) {
 	el.style.display = "block";
 }
 
-function fillin_fields(obj)
-{
+function fillin_fields(obj) {
 	fld_title.value = obj.title;
 	fld_subtitle.value = obj.subtitle;
 	fld_author.value = obj.author;
@@ -161,57 +143,69 @@ function fillin_fields(obj)
 	fld_ids.innerText = "Mediennummern: " + obj.ids.join(", ");
 }
 
-function update_suggestions(list)
-{
+function update_suggestions(list) {
 	let str =
 		"<tr>" +
-			"<th>LS</th>" +
-			"<th>Titel</th>" +
-			"<th>UTitel</th>" +
-			"<th>Autor</th>" +
-			"<th>Verlag</th>" +
-			"<th>ISBN</th>" +
-			"<th>EJahr</th>" +
-			"<th>MNummern</th>" +
+		"<th>LS</th>" +
+		"<th>Titel</th>" +
+		"<th>UTitel</th>" +
+		"<th>Autor</th>" +
+		"<th>Verlag</th>" +
+		"<th>ISBN</th>" +
+		"<th>EJahr</th>" +
+		"<th>MNummern</th>" +
 		"</tr>";
 
-	list.forEach(item => {
+	list.forEach((item) => {
 		str +=
 			"<tr>" +
-				"<td>" + item.ldist + "</td>" +
-				"<td>" + item.title + "</td>" +
-				"<td>" + item.subtitle + "</td>" +
-				"<td>" + item.author + "</td>" +
-				"<td>" + item.publisher + "</td>" +
-				"<td>" + item.isbn + "</td>" +
-				"<td>" + item.year + "</td>" +
-				"<td>" + item.ids.join(", ") + "</td>" +
+			"<td>" +
+			item.ldist +
+			"</td>" +
+			"<td>" +
+			item.title +
+			"</td>" +
+			"<td>" +
+			item.subtitle +
+			"</td>" +
+			"<td>" +
+			item.author +
+			"</td>" +
+			"<td>" +
+			item.publisher +
+			"</td>" +
+			"<td>" +
+			item.isbn +
+			"</td>" +
+			"<td>" +
+			item.year +
+			"</td>" +
+			"<td>" +
+			item.ids.join(", ") +
+			"</td>" +
 			"</tr>";
 	});
 
 	cont_suggestions.innerHTML = str;
 }
 
-function is_suggestion(cur, obj)
-{
+function is_suggestion(cur, obj) {
 	obj.ldist = 0;
 	return true;
 }
 
-function get_suggestions(cur)
-{
-	return data.bnew.filter(item => is_suggestion(cur, item))
+function get_suggestions(cur) {
+	return data.bnew
+		.filter((item) => is_suggestion(cur, item))
 		.sort((a, b) => a.ldist < b.ldist);
 }
 
-function action_skip()
-{
+function action_skip() {
 	data.old.push(data.old.shift());
 	first_book();
 }
 
-function action_new()
-{
+function action_new() {
 	let obj = {
 		title: fld_title.value,
 		subtitle: fld_subtitle.value,
@@ -219,7 +213,7 @@ function action_new()
 		publisher: fld_publisher.value,
 		isbn: fld_isbn.value,
 		year: parseInt(fld_year.value),
-		ids: data.old[0].ids
+		ids: data.old[0].ids,
 	};
 
 	data.old.shift();
@@ -227,69 +221,61 @@ function action_new()
 	first_book();
 }
 
-function first_book()
-{
-	if(data.old.length == 0)
-	{
+function first_book() {
+	if (data.old.length == 0) {
 		show(cont_congratulations);
 		hide(cont_cur_book_data);
 		action_save();
-	}
-	else
-	{
-		fld_progress.innerText = "TODO: " + data.old.length +
-			" - DONE: " + data.bnew.length;
+	} else {
+		fld_progress.innerText =
+			"TODO: " + data.old.length + " - DONE: " + data.bnew.length;
 		show(cont_cur_book_data);
 		let obj = data.old[0];
 		fillin_fields(obj);
 		update_suggestions(get_suggestions(obj));
+		update_g_suggestions(obj);
 	}
 }
 
-function parse_json(json)
-{
+function parse_json(json) {
 	data = json;
 	first_book();
 	hide(cont_file_upload);
 }
 
-function read_file(file)
-{
+function read_file(file) {
 	const reader = new FileReader();
-	reader.onload = function(event)
-	{
+	reader.onload = function (event) {
 		parse_json(JSON.parse(event.target.result));
 	};
 
 	reader.readAsText(file);
 }
 
-function padToFour(number)
-{
-	if(number <= 9999) { number = ("000" + number).slice(-4); }
+function padToFour(number) {
+	if (number <= 9999) {
+		number = ("000" + number).slice(-4);
+	}
 	return number;
 }
 
-function action_save()
-{
+function action_save() {
 	data.version += 1;
 
 	const link = document.createElement("a");
 	const content = JSON.stringify(data);
-	const file = new Blob([content], { type: 'text/plain' });
+	const file = new Blob([content], { type: "text/plain" });
 	link.href = URL.createObjectURL(file);
 	link.download = "bookdata_" + padToFour(data.version) + ".json";
 	link.click();
 	URL.revokeObjectURL(link.href);
 }
 
-file_sel.onchange = function()
-{
+file_sel.onchange = function () {
 	show(btn_upload);
 };
 
-btn_upload.onclick = function()
-{
+btn_upload.onclick = function () {
 	const file = file_sel.files[0];
 	read_file(file);
 };
@@ -302,3 +288,68 @@ btn_new.onclick = action_new;
 hide(btn_upload);
 hide(cont_cur_book_data);
 hide(cont_congratulations);
+
+const s_title = elem("#s_title");
+const s_subtitle = elem("#s_subtitle");
+const s_author = elem("#s_author");
+const s_publisher = elem("#s_publisher");
+const s_isbn = elem("#s_isbn");
+const s_year = elem("#s_year");
+const s_img = elem("#s_img");
+
+const btn_s_last = elem("#btn_s_last");
+const btn_s_next = elem("#btn_s_next");
+const btn_s_take = elem("#btn_s_take");
+
+async function update_g_suggestions(cur) {
+	// get data
+
+	let suggs = await get_api_suggestions(cur);
+
+	// render data
+	render_g_suggestions(suggs);
+}
+
+function render_g_suggestions(suggs) {
+	let curS = 0;
+	render_g_suggestion(suggs[curS], curS, suggs.length);
+
+	btn_s_next.onclick = () => {
+		curS++;
+		render_g_suggestion(suggs[curS], curS, suggs.length);
+	};
+	btn_s_last.onclick = () => {
+		curS--;
+		render_g_suggestion(suggs[curS], curS, suggs.length);
+	};
+	btn_s_take.onclick = () => {
+		fillin_suggestion(suggs[curS]);
+	};
+}
+function fillin_suggestion(obj) {
+	fld_title.value = obj.title;
+	fld_subtitle.value = obj.subtitle;
+	fld_author.value = obj.author;
+	fld_publisher.value = obj.publisher;
+	fld_isbn.value = obj.isbn;
+	fld_year.value = obj.year;
+}
+function render_g_suggestion(sugg, curS, suggs_cnt) {
+	btn_s_last.disabled = false;
+	btn_s_next.disabled = false;
+
+	if (curS == 0) {
+		btn_s_last.disabled = true;
+	}
+	if (curS >= suggs_cnt - 1) {
+		btn_s_next.disabled = true;
+	}
+
+	s_title.value = sugg.title;
+	s_subtitle.value = sugg.subtitle;
+	s_author.value = sugg.author;
+	s_publisher.value = sugg.publisher;
+	s_isbn.value = sugg.isbn;
+	s_year.value = sugg.year;
+	s_img.src = sugg.image;
+}
